6161e6cb22fa7f346899111f5e33b04036361ec9
Michael Fuhrer <mfuhrer@vt.edu>
2021-03-23 21:07:41 -0400

    Updated description.

--- article_handler.py
+++ article_handler.py
@@ -1,2 +1,4 @@
 # Author: Michael Fuhrer
 # Created: 2021-03-23   Last Updated: 2021-03-23
+#
+# Used by ner_fuhrer to open file/website based on user query.
--- ner_fuhrer.py
+++ ner_fuhrer.py
@@ -1,2 +1,5 @@
 # Author: Michael Fuhrer
 # Created: 2021-03-23   Last Updated: 2021-03-23
+#
+# Contains main function call for Grier-NER. Will handle and parse
+# user input and accordingly operate ner_core to produce desired output.
